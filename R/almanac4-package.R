2edfd146930c85ffc060eb585e745eccac24d35c
DavisVaughan <davis@rstudio.com>
2019-09-17 09:09:47 -0400

    Initial setup

---
+++ R/almanac4-package.R
@@ -0,0 +1,22 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @import rlang
+#' @import lubridate
+#' @importFrom glue glue
+#' @importFrom vctrs vec_size
+#' @importFrom vctrs vec_cast
+#' @importFrom vctrs vec_in
+#' @importFrom vctrs vec_assert
+#' @importFrom vctrs new_date
+#' @importFrom vctrs vec_match
+#' @importFrom vctrs vec_is
+#' @importFrom vctrs vec_init
+#' @importFrom vctrs vec_arith
+#' @importFrom vctrs vec_slice<-
+#' @importFrom vctrs maybe_lossy_cast
+## usethis namespace: end
+NULL
