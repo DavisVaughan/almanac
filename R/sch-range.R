87bec3590b56c654d2bde3611c568999b201bf69
DavisVaughan <davis@rstudio.com>
2019-09-30 09:04:40 -0400

    Range function docs

--- R/sch-range.R
+++ R/sch-range.R
@@ -1,3 +1,32 @@
+#' Generate events in a schedule
+#'
+#' `sch_seq()` generates all events in a schedule from `from` to `to`.
+#'
+#' @param from,to `[Date(1)]`
+#'
+#'    Dates defining the range to look for events in the `schedule`.
+#'
+#' @param schedule `[schedule / rrule]`
+#'
+#'    A schedule or rrule.
+#'
+#' @param inclusive `[logical(1)]`
+#'
+#'    If `from` or `to` are events, should they be included?
+#'
+#' @examples
+#' library(magrittr)
+#'
+#' on_12th <- monthly() %>% rr_on_mday(12)
+#' on_monday <- weekly() %>% rr_on_wday("Monday")
+#'
+#' # On the 12th of the month, or on Mondays
+#' sch <- schedule() %>%
+#'   sch_add_rrule(on_12th) %>%
+#'   sch_add_rrule(on_monday)
+#'
+#' sch_seq("2019-01-01", "2019-01-31", sch)
+#'
 #' @export
 sch_seq <- function(from, to, schedule, inclusive = TRUE) {
   from <- vec_cast_date(from)
@@ -20,6 +49,35 @@ sch_seq <- function(from, to, schedule, inclusive = TRUE) {
   parse_js_date(out)
 }
 
+#' Generate the next or previous event
+#'
+#' @description
+#'
+#' - `sch_next()` generates the next event after `x`.
+#'
+#' - `sch_previous()` generates the previous event before `x`.
+#'
+#' @inheritParams sch_seq
+#'
+#' @param x `[Date(1)]`
+#'
+#'    The date to start the search from.
+#'
+#' @examples
+#' library(magrittr)
+#'
+#' on_12th <- monthly() %>% rr_on_mday(12)
+#' on_monday <- weekly() %>% rr_on_wday("Monday")
+#'
+#' # On the 12th of the month, or on Mondays
+#' sch <- schedule() %>%
+#'   sch_add_rrule(on_12th) %>%
+#'   sch_add_rrule(on_monday)
+#'
+#' sch_next("2019-01-01", sch)
+#'
+#' sch_previous("2019-01-01", sch)
+#'
 #' @export
 sch_next <- function(x, schedule, inclusive = FALSE) {
   x <- vec_cast_date(x)
@@ -36,6 +94,7 @@ sch_next <- function(x, schedule, inclusive = FALSE) {
   parse_js_date(out)
 }
 
+#' @rdname sch_next
 #' @export
 sch_previous <- function(x, schedule, inclusive = FALSE) {
   x <- vec_cast_date(x)
