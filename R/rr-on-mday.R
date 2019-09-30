8086875dc5dd284f7b30b2360300f27c88c1d959
DavisVaughan <davis@rstudio.com>
2019-09-30 10:37:40 -0400

    `rr_on_mday()` docs

--- R/rr-on-mday.R
+++ R/rr-on-mday.R
@@ -1,13 +1,72 @@
+#' Recur on a day of the month
+#'
+#' `rr_on_mday()` recurs on a specific day of the month.
+#'
+#' @details
+#'
+#' If the day of the month doesn't exist for that particular month, then it
+#' is ignored. For example, if `rr_on_mday(30)` is set, then it will never
+#' generate an event in February.
+#'
+#' @param x `[rrule]`
+#'
+#'    A recurrence rule.
+#'
+#' @param mday `[integer]`
+#'
+#'    The days of the month on which to recur. Negative values are allowed,
+#'    which specify n days from the end of the month.
+#'
+#' @examples
+#' library(magrittr)
+#'
+#' # When used with a yearly or monthly frequency, `rr_on_mday()` expands the
+#' # number of days in the recurrence set.
+#' on_yearly <- yearly()
+#' on_yearly_mday_1_to_2 <- on_yearly %>% rr_on_mday(1:2)
+#'
+#' start <- "1999-01-01"
+#' end <- "2000-06-30"
+#'
+#' sch_seq(start, end, on_yearly)
+#' sch_seq(start, end, on_yearly_mday_1_to_2)
+#'
+#' # When used with a daily frequency, `rr_on_mday()` limits the number of
+#' # days in the recurrence set.
+#' on_daily <- daily()
+#' on_daily_mday_1_to_2 <- on_daily %>% rr_on_mday(1:2)
+#'
+#' length(sch_seq(start, end, on_daily))
+#' length(sch_seq(start, end, on_daily_mday_1_to_2))
+#'
+#' # Using a negative value is a powerful way to look back from the end of the
+#' # month. This is particularly useful because months don't have the same
+#' # number of days.
+#' on_last_of_month <- monthly() %>% rr_on_mday(-1)
+#'
+#' sch_seq(start, end, on_last_of_month)
+#'
+#' # If you want particular days of the week at the end of the month, you
+#' # could use something like this, which checks if the end of the month
+#' # is also a Friday.
+#' on_last_of_month_that_is_also_friday <- on_last_of_month %>% rr_on_wday("Friday")
+#' sch_seq(start, end, on_last_friday)
+#'
+#' # But you probably wanted this, which takes the last friday of the month,
+#' # on whatever day that lands on
+#' on_last_friday_of_month <- monthly() %>% rr_on_wday("Friday", -1)
+#' sch_seq(start, end, on_last_friday_of_month)
+#'
 #' @export
-rr_on_mday <- function(x, n) {
+rr_on_mday <- function(x, mday) {
   validate_rrule(x)
 
   old <- get_rule(x, "mday")
-  new <- vec_cast(n, integer(), x_arg = "n")
+  new <- vec_cast(mday, integer(), x_arg = "mday")
 
   abs_new <- abs(new)
   if (any(abs_new > 31 | abs_new < 1)) {
-    abort("`n` can only take values in [-31, -1] and [1, 31].")
+    abort("`mday` can only take values in [-31, -1] and [1, 31].")
   }
 
   new <- union(old, new)
