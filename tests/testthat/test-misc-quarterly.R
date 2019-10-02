c3e8ca9e6b7ca10d81400e63e3718896b43d9c51
DavisVaughan <davis@rstudio.com>
2019-10-02 10:47:00 -0400

    Add miscellaneous quarterly tests

---
+++ tests/testthat/test-misc-quarterly.R
@@ -0,0 +1,143 @@
+# ------------------------------------------------------------------------------
+
+make_nth_mday_of_the_quarter <- function(n) {
+  rr_nth_of_q1 <- yearly() %>% # It will be the n-th position in the set, per year
+    recur_on_ymonth(1:3) %>% # On all months in Q1
+    recur_on_mday(1:31) %>% # On all days of the month
+    recur_on_position(n) # The n-th day
+
+  rr_nth_of_q2 <- yearly() %>%
+    recur_on_ymonth(4:6) %>%
+    recur_on_mday(1:31) %>%
+    recur_on_position(n)
+
+  rr_nth_of_q3 <- yearly() %>%
+    recur_on_ymonth(7:9) %>%
+    recur_on_mday(1:31) %>%
+    recur_on_position(n)
+
+  rr_nth_of_q4 <- yearly() %>%
+    recur_on_ymonth(10:12) %>%
+    recur_on_mday(1:31) %>%
+    recur_on_position(n)
+
+  sch_nth_day_of_quarter <- schedule() %>%
+    sch_add_rrule(rr_nth_of_q1) %>%
+    sch_add_rrule(rr_nth_of_q2) %>%
+    sch_add_rrule(rr_nth_of_q3) %>%
+    sch_add_rrule(rr_nth_of_q4)
+
+  sch_nth_day_of_quarter
+}
+
+
+test_that("can construct a schedule to select n-th mday of the quarter", {
+  n <- 60L
+  start <- as.Date("2000-01-01")
+  stop <- as.Date("2001-12-31")
+
+  sch_60th_day_of_quarter <- make_nth_mday_of_the_quarter(n)
+
+  expect <- seq(start, stop, "1 day")
+  expect <- expect[qday(expect) == n]
+
+  x <- sch_seq(start, stop, sch_60th_day_of_quarter)
+
+  expect_equal(x, expect)
+})
+
+test_that("can select n-th mday of the quarter from the back", {
+  n <- -1
+
+  sch_neg_1th_day_of_quarter <- make_nth_mday_of_the_quarter(n)
+
+  x <- sch_seq("2000-01-01", "2001-12-31", sch_neg_1th_day_of_quarter)
+
+  expect <- as.Date(c(
+    "2000-03-31", "2000-06-30", "2000-09-30", "2000-12-31",
+    "2001-03-31", "2001-06-30", "2001-09-30", "2001-12-31"
+  ))
+
+  expect_equal(x, expect)
+})
+
+# ------------------------------------------------------------------------------
+
+make_nth_wday_of_the_quarter <- function(wday, n) {
+  rr_nth_wday_of_q1 <- yearly() %>% # It will be the n-th position in the set, per year
+    recur_on_ymonth(1:3) %>% # On all months in Q1
+    recur_on_wday(wday) %>% # On these weekdays
+    recur_on_position(n) # The n-th one
+
+  rr_nth_wday_of_q2 <- yearly() %>%
+    recur_on_ymonth(4:6) %>%
+    recur_on_wday(wday) %>%
+    recur_on_position(n)
+
+  rr_nth_wday_of_q3 <- yearly() %>%
+    recur_on_ymonth(7:9) %>%
+    recur_on_wday(wday) %>%
+    recur_on_position(n)
+
+  rr_nth_wday_of_q4 <- yearly() %>%
+    recur_on_ymonth(10:12) %>%
+    recur_on_wday(wday) %>%
+    recur_on_position(n)
+
+  sch_nth_wday_of_quarter <- schedule() %>%
+    sch_add_rrule(rr_nth_wday_of_q1) %>%
+    sch_add_rrule(rr_nth_wday_of_q2) %>%
+    sch_add_rrule(rr_nth_wday_of_q3) %>%
+    sch_add_rrule(rr_nth_wday_of_q4)
+
+  sch_nth_wday_of_quarter
+}
+
+test_that("can construct a schedule to select n-th wday of the quarter", {
+  n <- 6L
+  wday <- "Monday"
+  start <- as.Date("2000-01-01")
+  stop <- as.Date("2001-12-31")
+
+  sch_6th_monday_of_quarter <- make_nth_wday_of_the_quarter(wday, n)
+
+  x <- sch_seq(start, stop, sch_6th_monday_of_quarter)
+
+  expect <- as.Date(c(
+    "2000-02-07", "2000-05-08", "2000-08-07", "2000-11-06",
+    "2001-02-05", "2001-05-07", "2001-08-06", "2001-11-05"
+  ))
+
+  expect_equal(x, expect)
+})
+
+test_that("not all quarters might have the requested position", {
+  n <- 14
+  wday <- "Monday"
+
+  sch_14th_monday_of_quarter <- make_nth_wday_of_the_quarter(wday, n)
+
+  x <- sch_seq("2000-01-01", "2001-12-31", sch_14th_monday_of_quarter)
+
+  expect <- as.Date("2001-12-31") # <- the only quarter with a 14th monday
+
+  expect_equal(x, expect)
+})
+
+test_that("can select n-th wday in the quarter from the back", {
+  n <- -2
+  wday <- c("Monday", "Tuesday")
+
+  sch_neg_2nd_monday_or_tuesday_of_quarter <- make_nth_wday_of_the_quarter(wday, n)
+
+  x <- sch_seq("2000-01-01", "2001-12-31", sch_neg_2nd_monday_or_tuesday_of_quarter)
+
+  expect <- as.Date(c(
+    "2000-03-27", "2000-06-26", "2000-09-25", "2000-12-25",
+    "2001-03-26", "2001-06-25", "2001-09-24", "2001-12-25"
+  ))
+
+  expect_equal(x, expect)
+})
+
+# ------------------------------------------------------------------------------
