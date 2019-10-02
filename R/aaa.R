af01bada5afc29f637af7ecaaefa3d1798eb42e2
DavisVaughan <davis@rstudio.com>
2019-10-02 14:17:02 -0400

    Use a global empty date in `vec_cast_date()`

--- R/aaa.R
+++ R/aaa.R
@@ -1,10 +1,13 @@
 global_context <- NULL
+global_empty_date <- NULL
 
 .onLoad <- function(libname, pkgname) {
   rrule_js_path <- system.file("js/rrule.js", package = pkgname)
 
   global_context <<- V8::v8()
   global_context$source(rrule_js_path)
+
+  global_empty_date <<- vctrs::new_date()
 
   invisible()
 }
