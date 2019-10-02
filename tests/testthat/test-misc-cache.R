2502ff4d40a13bce623a406147a43ea1ad6b0df0
DavisVaughan <davis@rstudio.com>
2019-10-02 13:44:51 -0400

    Add cache test specifically for disjoint ranges

--- tests/testthat/test-misc-cache.R
+++ tests/testthat/test-misc-cache.R
@@ -9,3 +9,14 @@ test_that("cache stores events since `since`", {
 
   expect_equal(rrule$env$events, expect)
 })
+
+test_that("cache can handle disjoint repeated calls (#6)", {
+  rrule <- monthly()
+
+  sch_seq("1999-01-01", "2000-01-01", rrule)
+  sch_seq("2001-01-01", "2002-01-01", rrule)
+
+  expect <- as.Date(c("2000-05-01", "2000-06-01"))
+
+  expect_equal(sch_seq("2000-05-01", "2000-06-01", rrule), expect)
+})
