29f268b79aaab1cc80daf07fcf5d3fa50c3d8ba2
DavisVaughan <davis@rstudio.com>
2020-04-17 15:14:18 -0400

    Add `sch_events()`

---
+++ tests/testthat/test-sch-events.R
@@ -0,0 +1,15 @@
+test_that("can get events from a rrule", {
+  rrule <- daily(since = "1970-01-01", until = "1970-01-03")
+  expect_identical(sch_events(rrule), new_date(c(0, 1, 2)))
+})
+
+test_that("can get events from a calendar", {
+  rrule1 <- daily(since = "1970-01-01", until = "1970-01-02")
+  rrule2 <- daily(since = "1970-01-02", until = "1970-01-03")
+
+  sch <- schedule() %>%
+    sch_rrule(rrule1) %>%
+    sch_rrule(rrule2)
+
+  expect_identical(sch_events(sch), new_date(c(0, 1, 2)))
+})
