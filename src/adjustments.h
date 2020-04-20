b2d5bdeef442bcad9c7448fba762df54d48f1851
DavisVaughan <davis@rstudio.com>
2020-04-20 10:37:55 -0400

    Change header grouping

--- src/adjustments.h
+++ src/adjustments.h
@@ -4,18 +4,15 @@
 #include "r.h"
 
 sexp adj_following_impl(sexp x, sexp events);
-double adj_following_one(double x, double* p_begin, double* p_end);
-
 sexp adj_preceding_impl(sexp x, sexp events);
-double adj_preceding_one(double x, double* p_begin, double* p_end);
-
 sexp adj_modified_following_impl(sexp x, sexp events);
-double adj_modified_following_one(double x, double* p_begin, double* p_end);
-
 sexp adj_modified_preceding_impl(sexp x, sexp events);
-double adj_modified_preceding_one(double x, double* p_begin, double* p_end);
-
 sexp adj_nearest_impl(sexp x, sexp events);
+
+double adj_following_one(double x, double* p_begin, double* p_end);
+double adj_preceding_one(double x, double* p_begin, double* p_end);
+double adj_modified_following_one(double x, double* p_begin, double* p_end);
+double adj_modified_preceding_one(double x, double* p_begin, double* p_end);
 double adj_nearest_one(double x, double* p_begin, double* p_end);
 
 #endif
