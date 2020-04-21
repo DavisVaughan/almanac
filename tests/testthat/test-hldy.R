71c62a39819a636f2a12eb5d05242271c9c8a212
DavisVaughan <davis@rstudio.com>
2020-04-21 13:08:39 -0400

    Holiday generator is simpler again

--- tests/testthat/test-hldy.R
+++ tests/testthat/test-hldy.R
@@ -1,5 +1,5 @@
 test_that("can construct a hldy object", {
-  hldy <- new_hldy("hldy", function(x, y, w, z) x, function(x, y) x)
+  hldy <- new_hldy("hldy", function(x, y) x, function(x, y) x)
   expect_s3_class(hldy, "hldy")
   expect_named(hldy, c("name", "generator", "adjustment"))
 })
