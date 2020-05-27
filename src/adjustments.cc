b15d2cff052fc067a42b22ac52cf16444177db0e
DavisVaughan <davis@rstudio.com>
2020-05-27 15:01:34 -0400

    Use `std::isnan()`

--- src/adjustments.cc
+++ src/adjustments.cc
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include "months.h"
 #include <algorithm>
+#include <cmath>
 
 // -----------------------------------------------------------------------------
 
@@ -20,7 +21,7 @@
     const double elt = p_x[i];                                 \
                                                                \
     /* Handle `NA_real_` and `NaN` early (#72) */              \
-    if (isnan(elt)) {                                          \
+    if (std::isnan(elt)) {                                     \
       p_out[i] = elt;                                          \
       continue;                                                \
     }                                                          \
